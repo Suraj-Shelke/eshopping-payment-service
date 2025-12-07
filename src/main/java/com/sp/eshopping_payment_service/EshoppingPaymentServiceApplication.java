82043b0d53f2a27dc2cda0a7af49686d118abd79
suraj-shelke <shelkesuraj205@gmail.com>
2025-12-07 12:53:00 +0530

    initial setup

---
+++ src/main/java/com/sp/eshopping_payment_service/EshoppingPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sp.eshopping_payment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshoppingPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshoppingPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sp/eshopping_payment_service/EshoppingPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.eshopping_payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshoppingPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
