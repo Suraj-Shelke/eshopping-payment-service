d3fceb07837c95df82a41e929b9db1edbed73a63
suraj-shelke <shelkesuraj205@gmail.com>
2025-12-09 23:00:46 +0530

    logger implemented

--- src/main/java/com/sp/eshopping_payment_service/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/sp/eshopping_payment_service/service/impl/PaymentServiceImpl.java
@@ -7,6 +7,8 @@ import com.sp.eshopping_payment_service.payload.response.PaymentResponse;
 import com.sp.eshopping_payment_service.repository.TransactionDetailsRepository;
 import com.sp.eshopping_payment_service.service.PaymentService;
 import com.sp.eshopping_payment_service.utils.PaymentMode;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +19,12 @@ public class PaymentServiceImpl implements PaymentService {
     @Autowired
     private TransactionDetailsRepository transactionDetailsRepository;
 
+    Logger log= LoggerFactory.getLogger(PaymentServiceImpl.class);
     @Override
     public long doPayment(PaymentRequest paymentRequest) {
+
+        log.info("PaymentServiceImpl | doPayment is called");
+        log.info("PaymentServiceImpl | doPayment | Recording Payment Details: {}", paymentRequest);
 
         TransactionDetails transactionDetails = new TransactionDetails();
         transactionDetails.setOrderId(paymentRequest.getOrderId());
@@ -30,12 +36,15 @@ public class PaymentServiceImpl implements PaymentService {
 
         transactionDetails=transactionDetailsRepository.save(transactionDetails);
 
+        log.info("Transaction Completed with Id: {}", transactionDetails.getId());
         return transactionDetails.getId();
     }
 
     @Override
     public PaymentResponse getPaymentDetailsByOrderId(long orderId) {
 
+        log.info("PaymentServiceImpl | getPaymentDetailsByOrderId is called");
+        log.info("PaymentServiceImpl | getPaymentDetailsByOrderId | Getting payment details for the Order Id: {}", orderId);
         TransactionDetails transactionDetails
                 = transactionDetailsRepository.findByOrderId(orderId)
                 .orElseThrow(() -> new PaymentServiceCustomException(
@@ -52,6 +61,7 @@ public class PaymentServiceImpl implements PaymentService {
         paymentResponse.setStatus(transactionDetails.getPaymentStatus());
         paymentResponse.setAmount(transactionDetails.getAmount());
 
+        log.info("PaymentServiceImpl | getPaymentDetailsByOrderId | paymentResponse: {}", paymentResponse.toString());
         return paymentResponse;
     }
 }
