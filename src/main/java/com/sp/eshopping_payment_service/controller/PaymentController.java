cc65527c99dce92ed04e3ca4ce6fa3f9d60cfe1a
suraj-shelke <shelkesuraj205@gmail.com>
2025-12-09 22:54:00 +0530

    logger implemented

--- src/main/java/com/sp/eshopping_payment_service/controller/PaymentController.java
+++ src/main/java/com/sp/eshopping_payment_service/controller/PaymentController.java
@@ -3,6 +3,8 @@ package com.sp.eshopping_payment_service.controller;
 import com.sp.eshopping_payment_service.payload.request.PaymentRequest;
 import com.sp.eshopping_payment_service.payload.response.PaymentResponse;
 import com.sp.eshopping_payment_service.service.PaymentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +16,12 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    Logger log= LoggerFactory.getLogger(PaymentController.class);
     @PostMapping
     public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest) {
+
+        log.info("PaymentController | doPayment is called");
+        log.info("PaymentController | doPayment | paymentRequest : " + paymentRequest.toString());
         return new ResponseEntity<>(
                 paymentService.doPayment(paymentRequest),
                 HttpStatus.OK
@@ -24,6 +30,8 @@ public class PaymentController {
 
     @GetMapping("/order/{orderId}")
     public ResponseEntity<PaymentResponse> getPaymentDetailsByOrderId(@PathVariable long orderId) {
+        log.info("PaymentController | getPaymentDetailsByOrderId is called");
+        log.info("PaymentController | getPaymentDetailsByOrderId | orderId : " + orderId);
         return new ResponseEntity<>(
                 paymentService.getPaymentDetailsByOrderId(orderId),
                 HttpStatus.OK
