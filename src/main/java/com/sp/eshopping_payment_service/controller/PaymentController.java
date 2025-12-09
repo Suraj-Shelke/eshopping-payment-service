a46a012ffbb0f59ad2598616b10d79e48558c93d
suraj-shelke <shelkesuraj205@gmail.com>
2025-12-09 22:50:28 +0530

    created endpoints

---
+++ src/main/java/com/sp/eshopping_payment_service/controller/PaymentController.java
@@ -0,0 +1,32 @@
+package com.sp.eshopping_payment_service.controller;
+
+import com.sp.eshopping_payment_service.payload.request.PaymentRequest;
+import com.sp.eshopping_payment_service.payload.response.PaymentResponse;
+import com.sp.eshopping_payment_service.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest) {
+        return new ResponseEntity<>(
+                paymentService.doPayment(paymentRequest),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/order/{orderId}")
+    public ResponseEntity<PaymentResponse> getPaymentDetailsByOrderId(@PathVariable long orderId) {
+        return new ResponseEntity<>(
+                paymentService.getPaymentDetailsByOrderId(orderId),
+                HttpStatus.OK
+        );
+    }
+}
